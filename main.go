bf6f95f853ca2b82b529caca05da89e29c206fc6
buzzbait <buzzbait@naver.com>
2024-12-31 15:37:27 +0900

    기초환경 설정

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+
+	"github.com/buzzbait/gorest/initializers"
+	"github.com/gofiber/fiber/v2"
+)
+
+func init() {
+	config, err := initializers.LoadConfig(".")
+	if err != nil {
+		log.Fatalln("Failed to load environment variables! \n", err.Error())
+	}
+	initializers.ConnectDB(&config)
+}
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/api/healthchecker", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(fiber.Map{
+			"status":  "success",
+			"message": "Welcome to Golang, Fiber, and GORM",
+		})
+	})
+
+	log.Fatal(app.Listen(":8000"))
+}
