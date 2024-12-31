cea5542c8777aed612acfbff3cf3aa2fa0c948b2
buzzbait <buzzbait@naver.com>
2024-12-31 23:03:38 +0900

    컨트롤러 구현

--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"log"
 
+	"github.com/buzzbait/gorest/controllers"
 	"github.com/buzzbait/gorest/initializers"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func init() {
@@ -17,11 +20,30 @@ func init() {
 
 func main() {
 	app := fiber.New()
+	micro := fiber.New()
 
-	app.Get("/api/healthchecker", func(c *fiber.Ctx) error {
+	app.Mount("/api", micro)
+	app.Use(logger.New())
+	app.Use(cors.New(cors.Config{
+		AllowOrigins:     "http://localhost:3000",
+		AllowHeaders:     "Origin, Content-Type, Accept",
+		AllowMethods:     "GET, POST, PATCH, DELETE",
+		AllowCredentials: true,
+	}))
+
+	micro.Route("/notes", func(router fiber.Router) {
+		router.Post("/", controllers.CreateNoteHandler)
+		router.Get("", controllers.FindNotes)
+	})
+	micro.Route("/notes/:noteId", func(router fiber.Router) {
+		router.Delete("", controllers.DeleteNote)
+		router.Get("", controllers.FindNoteById)
+		router.Patch("", controllers.UpdateNote)
+	})
+	micro.Get("/healthchecker", func(c *fiber.Ctx) error {
 		return c.Status(200).JSON(fiber.Map{
 			"status":  "success",
-			"message": "Welcome to Golang, Fiber, and GORM",
+			"message": "Welcome to Golang, Fiber, MySQL, and GORM",
 		})
 	})
 
